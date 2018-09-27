b2cb254f0cd2e7b41aa5701356c8be59dd88800d
Aidan Li <ali0100@myduneland.org>
2018-09-27 16:26:56 -0500

    Initial sharing of project

---
+++ Lion.java
@@ -0,0 +1,19 @@
+
+/**
+ * A class of the zoo project--lion
+ *
+ * Aidan Li
+ * Version 0
+ */
+
+    public class Lion
+{
+    private int age;
+
+public void roar()
+        {
+            System.out.println("Roar! Roar!");
+    } // roar() end
+} //Lion end
+
+
---
+++ ZooProgram.java
@@ -0,0 +1,14 @@
+
+/**
+ * The zoo program
+ *
+ * Aidan Li
+ * zippity zop
+ */
+public class ZooProgram {
+  	public static void main (String [] args) {
+   		Lion leo = new Lion ();
+		leo.roar();
+	} // main () end
+} // ZooProgram end
+
